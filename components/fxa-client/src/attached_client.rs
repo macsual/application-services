6df1e3c72c115277c53f24313e09157cf8ccfbdb
lougeniac64 <lougeniaC64@users.noreply.github.com>
2020-04-27 16:33:18 -0400

    Moved attached clients tests

--- components/fxa-client/src/attached_client.rs
+++ components/fxa-client/src/attached_client.rs
@@ -45,3 +45,101 @@ impl FirefoxAccount {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        config::Config,
+        http_client::{DeviceType, FxAClientMock},
+        oauth::{RefreshToken},
+    };
+    use std::{
+        collections::{HashSet},
+        sync::Arc,
+    };
+
+    #[test]
+    fn test_get_attached_clients() {
+        let config = Config::stable_dev("12345678", "https://foo.bar");
+        let mut fxa = FirefoxAccount::with_config(config);
+
+        fxa.state.refresh_token = Some(RefreshToken {
+            token: "refreshtok".to_string(),
+            scopes: HashSet::default(),
+        });
+
+        let mut client = FxAClientMock::new();
+        client
+            .expect_attached_clients(mockiato::Argument::any, |token| {
+                token.partial_eq("refreshtok")
+            })
+            .times(1)
+            .returns_once(Ok(vec![AttachedClient {
+                client_id: Some("12345678".into()),
+                session_token_id: None,
+                refresh_token_id: Some("refreshtok".into()),
+                device_id: None,
+                device_type: Some(DeviceType::Desktop),
+                is_current_session: true,
+                name: None,
+                created_time: None,
+                last_access_time: None,
+                scope: None,
+                user_agent: "attachedClientsUserAgent".into(),
+                os: None,
+            }]));
+
+        fxa.set_client(Arc::new(client));
+        assert!(fxa.attached_clients_cache.is_none());
+
+        let res = fxa.get_attached_clients();
+
+        assert!(res.is_ok());
+        assert!(fxa.attached_clients_cache.is_some());
+
+        let cached_attached_clients_res = fxa.attached_clients_cache.unwrap();
+        assert!(!cached_attached_clients_res.response.is_empty());
+        assert!(cached_attached_clients_res.cached_at > 0);
+
+        let cached_attached_client = &cached_attached_clients_res.response[0];
+        assert_eq!(
+            cached_attached_client.clone().client_id.unwrap(),
+            "12345678".to_string()
+        );
+    }
+
+    #[test]
+    fn test_get_attached_clients_network_errors() {
+        let config = Config::stable_dev("12345678", "https://foo.bar");
+        let mut fxa = FirefoxAccount::with_config(config);
+
+        fxa.state.refresh_token = Some(RefreshToken {
+            token: "refreshtok".to_string(),
+            scopes: HashSet::default(),
+        });
+
+        let mut client = FxAClientMock::new();
+        client
+            .expect_attached_clients(mockiato::Argument::any, |token| {
+                token.partial_eq("refreshtok")
+            })
+            .times(1)
+            .returns_once(Err(ErrorKind::RemoteError {
+                code: 500,
+                errno: 101,
+                error: "Did not work!".to_owned(),
+                message: "Did not work!".to_owned(),
+                info: "Did not work!".to_owned(),
+            }
+            .into()));
+
+        fxa.set_client(Arc::new(client));
+        assert!(fxa.attached_clients_cache.is_none());
+
+        let res = fxa.get_attached_clients();
+        assert!(res.is_err());
+        assert!(fxa.attached_clients_cache.is_none());
+    }
+}
+
